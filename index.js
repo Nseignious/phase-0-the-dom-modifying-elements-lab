dc5ee217f8367c3fffc6951e5a6633bf4046b37d
Aneya Seignious <nseignious@gmail.com>
2023-04-24 17:46:49 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove()
+const h1 = document.createElement("h1");
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.textContent = "Aneya is the champion"
